aba910e8ed1a2919a8aa9f5684290a58063bb018
Madhushree Kunder <65384355+MadhushreeKunder@users.noreply.github.com>
2022-02-11 06:39:04 +0000

    add post model

---
+++ models/post.model.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+const { Schema} = mongoose;
+
+const PostSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'SocialProfile',
+      },
+
+      caption: String,
+
+      content: {
+        type: String,
+        required: 'Content for post is required'
+      },
+
+      media: {
+        type: String,
+        default: '',
+      },
+
+      likes: [
+        {
+          type: Schema.Types.ObjectId, ref: 'SocialProfile'
+        }
+      ],  
+  }, {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = { Post };
