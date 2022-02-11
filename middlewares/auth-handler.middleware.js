91aff55cc7387f1dfa43b96529f57c11506a27ad
Madhushree Kunder <65384355+MadhushreeKunder@users.noreply.github.com>
2022-02-11 08:18:29 +0000

    add auth handler

---
+++ middlewares/auth-handler.middleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const jwt_secret = process.env['JWT_SECRET'];
+
+const authVerify = (req, res, next) => {
+
+  try {
+    const token = req.headers.authorization;
+    const decoded = jwt.verify(token, jwt_secret);
+
+    userId: decoded.userId;
+    const user = await User.findById(userId);
+
+    if (!user){
+    return res.status(401).json({message: "Unauthorised access"});
+    }
+    req.user = user;
+    next();
+
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Unauthorised access", errMessage: error.message})
+  }
+};
+
+module.exports = { authVerify };
