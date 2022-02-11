8c41c82bd5e251bc01d629ade509292648f7fb4f
Madhushree Kunder <65384355+MadhushreeKunder@users.noreply.github.com>
2022-02-11 10:44:30 +0000

    add login

--- routes/social-profile.router.js
+++ routes/social-profile.router.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const jwt_secret = process.env['JWT_SECRET'];
+const bcrypt = require("bcrypt");
+const { User} = require("../models/user.model");
+const {SocialProfile} = require("../models/social-profile.model");
 
 
 
@@ -41,4 +46,59 @@ router.post('/signup', async(req, res) => {
       });
     }
   }
-})
+});
+
+router.post("/login", async(req, res) => {
+  try {
+    const email = req.get('email');
+    const password = req.get('password');
+    const user = await User.findOne({email});
+
+    if(!user){
+      res.status(403).json({
+        success: false,
+        message: "Email or password is incorrect"
+      });
+      return;
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if(!validPassword){
+      res.status(403).json({ 
+        success: false,
+        message: "Email or password is incorrect"
+      })
+      return;
+    }
+
+    const socialProfile = await SocialProfile.findOne({ userId: user._id})
+
+    if (!socialProfile){
+      res.status(403).json({success: false, message: "User does not exist. Please Sign up."});
+      return;
+    }
+
+    const token = jwt.sign({ userId: user._id}, jwt_secret, {expiresIn: '24h'});
+
+    res.status(200).json({
+      success: true,
+      response: {
+        name: user.username,
+        token, 
+        userId: socialProfile._id,
+        userName: socialProfile.userName,
+        avatar: socialProfile.avatar,
+      },
+    });
+
+  } catch(error){
+    console.log(error)
+    res.status(500).json({
+      success: false
+      message: "Something went wrong!",
+      errorMessage: error.message
+    });
+  }
+});
+
