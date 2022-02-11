5d2bb4f79fb05016ece75bd269666b2ab85a7c93
Madhushree Kunder <65384355+MadhushreeKunder@users.noreply.github.com>
2022-02-11 21:58:24 +0000

    add add routes middleware to index

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const { initialiseDBConnection } = require("./db/db.connect.js")
+
+require('dotenv').config();
+
+const users = require('./routes/user.router');
+const socialProfiles = require('./routes/social-profile.router');
+const posts = require('./routes/posts.router');
+
+const { authVerify } = require("./middlewares/auth-handler.middleware")
+const { errorHandler } = require("./middlewares/error-handler.middleware")
+const { routeNotFound } = require("./middlewares/route-not-found.middleware")
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+initialiseDBConnection();
+
+app.get('/', (req, res) => {
+  res.json({hello: 'Welcome to Coral-gram'});
+});
+
+app.use('/users', users);
+
+app.use('/social-profiles', socialProfiles);
+app.use('/posts', posts);
+
+app.use(authVerify);
+
+app.use(errorHandler);
+app.use(routeNotFound);
+
+app.listen(PORT , () => {
+  console.log("Listening on Port:", PORT)
+})
+
